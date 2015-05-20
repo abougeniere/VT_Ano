8aed8b628b32c53cfb93b5273bd57b9321a49541
abougeniere <abougeniere@yahoo.fr>
2015-05-20 11:09:03 +0200

    ok

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ require "env.php";
     <title><?php echo APPNAME; ?></title>
 
 
-
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
@@ -34,8 +33,9 @@ require "env.php";
 
 <?php
 
-/*
+//
 // Server connection
+//
 $link = mysql_connect(DATABASE_SERVER, USER, PASSWORD)
 or die("Impossible de se connecter : " . mysql_error());
 
@@ -45,57 +45,86 @@ echo 'Connexion au serveur ' . DATABASE_SERVER . ' OK<hr>';
 // Database connection
 $db_selected = mysql_select_db(DATABASE, $link);
 if (!$db_selected) {
-    die ('Impossible de sélectionner la base de données ' . DATABASE .' : ' . mysql_error());
+    die ('Impossible de sélectionner la base de données ' . DATABASE . ' : ' . mysql_error());
 }
 echo 'Connexion à la base ' . DATABASE . ' OK<hr>';
 
 // source Table creation
-$TABLE_COPY_NAME = TABLE . '_' . date("dmY_His");
-$result =  mysql_query('CREATE TABLE ' . $TABLE_COPY_NAME .' LIKE ' . TABLE . ';');
-if (!$result) {
-    die('Requête invalide : ' . mysql_error());
+$TABLE_COPY_NAME = TABLE . '_ANONYM';
+
+// Check table
+if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '". $TABLE_COPY_NAME ."'"))==0) {
+
+
+    $result = mysql_query('CREATE TABLE ' . $TABLE_COPY_NAME . ' LIKE ' . TABLE . ';');
+    if (!$result) {
+        die('Requête invalide : ' . mysql_error());
+    }
+    echo 'Création de ' . $TABLE_COPY_NAME . ' OK<br>';
+
+
+// Duplicate datas
+    $result = mysql_query('INSERT INTO ' . $TABLE_COPY_NAME . ' SELECT * FROM ' . TABLE . ';');
+    if (!$result) {
+        die('Requête invalide : ' . mysql_error());
+    }
+    echo 'Copie des données vers ' . $TABLE_COPY_NAME . ' OK<br>';
+
 }
-echo 'Création de ' . $TABLE_COPY_NAME . ' OK<br>';
+else
+    echo 'La table ' . $TABLE_COPY_NAME . ' existe déjà : OK<br>';
 
 
-//INSERT INTO table_destination SELECT * FROM table_source ;
-$result =  mysql_query('INSERT INTO ' . $TABLE_COPY_NAME . ' SELECT * FROM ' . TABLE . ';');
-if (!$result) {
-    die('Requête invalide : ' . mysql_error());
-}
-echo 'Copie des données vers ' . $TABLE_COPY_NAME . ' OK<br>';
 
-
-*/
+//
+// Anonymizer
+//
+echo "Anonymisation .";
 $faker = Faker\Factory::create('fr_FR'); // create a French faker
 
+$indexRow = 'codeUtilisateur';
 
-for ($i = 0; $i < 10; $i++) {
-    $lastName = cleanString($faker->lastName);
-    $firstName = cleanString($faker->firstName);
-    //$firstName = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $faker->firstName);
-
-    $query = 'UPDATE ' . TABLE . ' SET nom=' . $lastName . ', prenom=' . $firstName . ', ';
-
-    // default email for every rows
-    if (DEFAULT_EMAIL != "")
-        $query .= 'email=' . DEFAULT_EMAIL . ' ';
-    else
-        $query .= 'email=' . $firstName . '.' . $lastName . '@univ-evry.fr ';
-
-    echo $query;
+$tableResult = mysql_query('SELECT ' . $indexRow . ' FROM ' . $TABLE_COPY_NAME . ';');
+if (!$tableResult) {
+    die('Requête invalide : ' . mysql_error());
 }
 
 
+while ($row = mysql_fetch_array($tableResult, MYSQL_ASSOC)) {
+
+    $lastName = cleanString($faker->lastName);
+    $firstName = cleanString($faker->firstName);
+
+    $query = 'UPDATE ' . $TABLE_COPY_NAME . ' SET nom="' . $lastName . '", prenom="' . $firstName . '", ';
+
+    // default email for every row
+    if (DEFAULT_EMAIL != "")
+        $query .= 'email="' . DEFAULT_EMAIL . '" ';
+    else
+        $query .= 'email="' . $firstName . '.' . $lastName . '@univ-evry.fr" ';
+
+
+    $query .= 'WHERE ' . $indexRow . ' = ' . $row[$indexRow];
+
+
+    echo ".";
+
+    // Query exec
+    $result = mysql_query($query);
+    if (!$result) {
+        die('Requête invalide : ' . mysql_error());
+    }
+}
+echo " OK";
 
 // remove é,à,..
 function cleanString($str)
 {
     return (str_replace(
         array('à', 'â', 'ä', 'á', 'ã', 'å', 'î', 'ï', 'ì', 'í', 'ô', 'ö', 'ò', 'ó', 'õ', 'ø', 'ù', 'û', 'ü', 'ú', 'é', 'è', 'ê', 'ë', 'ç', 'ÿ', 'ñ',
-              'À', 'Â', 'Ä', 'Á', 'Ã', 'Å', 'Î', 'Ï', 'Ì', 'Í', 'Ô', 'Ö', 'Ò', 'Ó', 'Õ', 'Ø', 'Ù', 'Û', 'Ü', 'Ú', 'É', 'È', 'Ê', 'Ë', 'Ç', 'Ÿ', 'Ñ'),
+            'À', 'Â', 'Ä', 'Á', 'Ã', 'Å', 'Î', 'Ï', 'Ì', 'Í', 'Ô', 'Ö', 'Ò', 'Ó', 'Õ', 'Ø', 'Ù', 'Û', 'Ü', 'Ú', 'É', 'È', 'Ê', 'Ë', 'Ç', 'Ÿ', 'Ñ'),
         array('a', 'a', 'a', 'a', 'a', 'a', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'e', 'e', 'e', 'e', 'c', 'y', 'n',
-              'A', 'A', 'A', 'A', 'A', 'A', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'E', 'E', 'E', 'E', 'C', 'Y', 'N'),
+            'A', 'A', 'A', 'A', 'A', 'A', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'E', 'E', 'E', 'E', 'C', 'Y', 'N'),
         $str));
 }
 
